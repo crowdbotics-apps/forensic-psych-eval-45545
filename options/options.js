b8203dc5887a5d807b6286089107e5fb4de7dcfc
Crowdbotics <team@crowdbotics.com>
2024-01-25 18:30:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "forensic-psych-eval-45545",
+  url: "https://forensic-psych-eval-45545.botics.co",
+  api: "https://forensic-psych-eval-45545.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
